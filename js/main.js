ba3a63746a49f0583954c5d8bd39cc27d2ec6eb5
Maxine Ellah <maxine.ellah@gmail.com>
2016-02-29 15:33:59 +1300

    add table and buttons

---
+++ js/main.js
@@ -0,0 +1,6 @@
+//write event listeners for when each button is clicked, 
+//an action is performed;
+//when numbers are clicked, they are displayed
+//when symbols are clicked they are displayed
+//when = is clicked after an equation a function is called
+//when AC is clicked display area becomes empty (an empty string?)
