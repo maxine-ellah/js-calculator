49ad4bf583f86421ee1cf31302ecd62d5a689cb9
Heidi Goosen <heidi.goosen@gmail.com>
2016-03-02 13:26:41 +1300

    two more functions

--- js/test.js
+++ js/test.js
@@ -1,3 +1,5 @@
+var display = "0"; //value of display
+var displayLength = 8;  //maximum number of decimals before digit
 
 function run1() 
 {document.frm.total.value += "1";}
@@ -29,6 +31,8 @@ function run9()
 function run0() 
 {document.frm.total.value += "0";}
 
+function runDec()     //decimal, used runDec instead of .
+{document.frm.total.value += ".";}  
 
 function runplus() 
 {document.frm.total.value += "+";}
@@ -66,3 +70,19 @@ function evalu()
   var evalo = eval(document.frm.total.value) 
   document.frm.total.value = evalo;
 }
+
+
+//HEIDI
+//if current length is greater than 8... error message
+if(total.length > displayLength){
+    document.frm.total.value = "Too Long";
+    console.log(toolong);
+  }
+
+
+// One decimal place only
+function runDec(){
+if (current.length == 0){
+  total = "0."};
+};
+
